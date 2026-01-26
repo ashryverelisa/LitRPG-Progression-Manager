1a39bce2befaaad46c2a4301c728ff9a9efa9423
ElisaAshryver <elisaashryver@hotmail.com>
2026-01-26 16:43:24 +0100

    Init

---
+++ src/ProgressionManager/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ProgressionManager.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
